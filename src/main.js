10f8ae769e5f665fc0f388dff24f1ee7cdecbcee
Bluice Zhen <bluice.zhen@gmail.com>
2017-08-01 18:36:52 +0800

    feat: pageView of Register

--- src/main.js
+++ src/main.js
@@ -7,12 +7,18 @@ Vue.use(VueRouter);
 Vue.use(ElementUI);
 
 import pageUpload from './pages/Upload.vue';
+import pageReg from './pages/Reg.vue';
+
 
 const router = new VueRouter({
   routes: [
     {
       path: '/upload',
       component: pageUpload
+    },
+    {
+      path: '/reg',
+      component: pageReg
     }
   ]
 });
---
+++ src/pages/Reg.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="page-reg">
+    <div class="main">
+      <div class="head">
+        <h1>Register - Marmot</h1>
+        <el-form ref="form" :model="reg_info" @submit.prevent.self="reg()">
+          <el-form-item label="Username:">
+            <el-input v-model="reg_info.name" type="text"></el-input>
+          </el-form-item>
+          <el-form-item label="Password:">
+            <el-input v-model="reg_info.name" type="password"></el-input>
+          </el-form-item>
+          <el-form-item label="Repeat Password:">
+            <el-input v-model="reg_info.name" type="password"></el-input>
+          </el-form-item>
+          <el-form-item label=" ">
+            <el-input type="submit" value="Register"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        reg_info: {
+          username: '',
+          password: '',
+          password_re: ''
+        }
+      }
+    },
+    methods: {
+      reg() {
+        alert(11111111111111111111111);
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '../assets/scss/main';
+
+  #page-reg {
+    .main {
+      width: 400px;
+      margin: 100px auto;
+      padding: 20px;
+      background-color: $color-gray-light;
+      border-radius: 4px;
+
+      .head {
+        width: 100%;
+        text-align: center;
+
+        h1 {
+          font-size: 23px;
+          font-weight: lighter;
+          color: $color-black;
+        }
+      }
+    }
+  }
+</style>
